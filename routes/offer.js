b23de19fceb716ddbb68757ce8b3e3fd1f1cb653
Ibrahim Ansari <iamibrahimansari@gmail.com>
2023-04-28 18:49:48 +0530

    first commit

---
+++ routes/offer.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const {addOrUpdateOffer, getOffers, deleteOffer} = require('../controllers/offer');
+
+router.get('/', getOffers);
+router.put('/:id', addOrUpdateOffer);
+router.delete('/:id', deleteOffer);
+
+module.exports = router;
